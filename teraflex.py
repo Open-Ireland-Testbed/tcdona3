cc7f3d073ebc91ac4508f774c35f93e734c5c7b9
harL06 <harryleddy92@gmail.com>
2025-07-22 15:04:59 +0000

    Updated teraflex return_current_config() to stop it returning xml

--- teraflex.py
+++ teraflex.py
@@ -177,8 +177,8 @@ class TFlex:
         :return: dictionary containing the current configuration
         :rtype: dict
         """
-
-        return self._config
+        elem = self._config
+        return etree.tostring(elem, pretty_print=True, encoding="utf-8")
 
     def __get_config(self):  # This should be done in a more efficient way
         response = self.get_interface()
