59b5f1d1ea8b73cbc7c422857287fcd70db09579
harL06 <harryleddy92@gmail.com>
2025-07-24 14:21:26 +0000

    Remove test prints to avoid spam

--- teraflex_paramiko.py
+++ teraflex_paramiko.py
@@ -104,9 +104,9 @@ class TeraflexSSH:
         raw_otsi  = self._send(f"show interface {full_if}                   otsia otsi 1 pm current")
         raw_fec   = self._send(f"show interface {full_if}                   otuc4-p pm current")
 
-        print(f"RAW PHY: {raw_phy}")
-        print(f"RAW OTSI: {raw_otsi}")
-        print(f"RAW FEC: {raw_fec}")
+        # print(f"RAW PHY: {raw_phy}")
+        # print(f"RAW OTSI: {raw_otsi}")
+        # print(f"RAW FEC: {raw_fec}")
 
         results = {
             "rx_power":      None,
